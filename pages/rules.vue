79ac1ecbbf0d556599d565ef83b27a276da52d29
Masked <trist13579@gmail.com>
2024-06-24 23:03:23 -0400

    rules

--- pages/rules.vue
+++ pages/rules.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    Rules
-  </div>
+  <UCard class="rounded-2xl ml-[10%] mr-[10%] min-h-96">
+    <div class="flex flex-col place-items-center">
+      <div class="text-4xl font-bold m-4 text-primary">Rules</div>
+      <div class="m-2">Puppet Population is 2.5x</div>
+      <div class="m-2">Radiation Zone</div>
+      <div class="m-2">Safe Zone</div>
+      <div class="m-2">Traders</div>
+      <div class="m-2">Smaller Squad Sizes</div>
+    </div>
+  </UCard>
 </template>
 
 <script lang="ts" setup>
