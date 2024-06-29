742f4b1301087c8e3fc949492bd89069c3a392d4
Masked <trist13579@gmail.com>
2024-06-28 21:51:26 -0400

    Evenets Page Layout

--- pages/events.vue
+++ pages/events.vue
@@ -1,7 +1,43 @@
 <template>
   <UCard class="rounded-2xl ml-[10%] mr-[10%] min-h-96">
     <div class="flex flex-col place-items-center">
-      Events Coming Soon!
+      <div class="text-3xl font-bold text-primary m-2">Events</div>
+      <UCard class="m-2 min-w-[90%]">
+        <template #header>
+          <UIcon name="i-heroicons-home-modern" class="text-xl text-primary"/> Critter's Second Home
+        </template>
+        <UProgress animation="carousel" />
+        <template #footer>
+          <div class="flex flex-row justify-between">
+            <UBadge size="md" color="yellow" variant="solid">Under Constructioin</UBadge>
+            <div class="font-bold text-primary">N/A</div>
+          </div>
+        </template>
+      </UCard>
+      <UCard class="m-2 min-w-[90%]">
+        <template #header>
+          <UIcon name="i-heroicons-home" class="text-xl text-primary"/> Critter's Frozen Vacation Home
+        </template>
+        <div>Information Coming Soon</div>
+        <template #footer>
+          <div class="flex flex-row justify-between">
+            <UBadge size="md" color="green" variant="solid">Unraided</UBadge>
+            <div class="font-bold text-primary">Posted: 6/25/2024</div>
+          </div>
+        </template>
+      </UCard>
+      <UCard class="m-2 min-w-[90%]">
+        <template #header>
+          <UIcon name="i-heroicons-building-storefront" class="text-xl text-primary"/> Critter's Hidden Toolshed
+        </template>
+        <div>Information Coming Soon</div>
+        <template #footer>
+          <div class="flex flex-row justify-between">
+            <UBadge size="md" color="red" variant="solid">Raided</UBadge>
+            <div class="font-bold text-primary">Posted: 6/24/2024</div>
+          </div>
+        </template>
+      </UCard>
     </div>
   </UCard>
 </template>
